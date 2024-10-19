a53f0e99bb631ab3d362e6324895c269d43e6eb5
AronCao49 <haozhi001@e.ntu.edu.sg>
2024-10-19 12:32:23 +0800

    Bug fixed for result saving

--- python/kiss_icp/pipeline.py
+++ python/kiss_icp/pipeline.py
@@ -232,6 +232,7 @@ class OdometryPipeline:
     def _create_output_dir(self):
         # update 
         if self.out_dir:
-            self.results_dir = self._get_results_dir(self.config.out_dir)
+            self.results_dir = self.out_dir
         else:
-            self.results_dir = self._get_results_dir(self.out_dir)
+            self.results_dir = self._get_results_dir(self.config.out_dir)
+            
